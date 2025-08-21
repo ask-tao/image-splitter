c18b7cb5b239183be80b5d4e193335623a39069a
zhang <735162787@qq.com>
2025-08-21 17:39:33 +0800

    fixt:内联favicon，解决单文件不显示icon

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
+import favicon from '../icon/logo.svg?inline';
+
+const setFavicon = (favImg: string) => {
+  const head = document.querySelector('head');
+  if (head) {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/svg+xml';
+    link.href = favImg;
+    head.appendChild(link);
+  }
+};
+
+setFavicon(favicon);
+
 import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
